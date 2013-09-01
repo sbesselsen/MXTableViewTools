6f8652b75df69d2ae8f2d8eda55bffb17bc288ed
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2013-09-01 18:46:22 +0200

    Initial Commit

---
+++ MXTableViewTools/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MXTableViewTools
+//
+//  Created by Sebastiaan Besselsen on 01-09-13.
+//  Copyright (c) 2013 ExtendD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
